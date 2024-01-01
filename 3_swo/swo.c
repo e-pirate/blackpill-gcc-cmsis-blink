b7aa57470ef2653a6042077687edb4215e1493f6
e-pirate <37085065+e-pirate@users.noreply.github.com>
2024-01-01 16:32:53 +0300

    First working version.

--- 3_swo/swo.c
+++ 3_swo/swo.c
@@ -1,7 +1,5 @@
 #include "swo.h"
 
-//#define DEMCR *((volatile uint32_t*) 0xE000EDFCu)
-
 void SWO_init(uint8_t STM_port)
 {
     // Only values 0-31 are valid for stimulus port number
@@ -10,21 +8,26 @@ void SWO_init(uint8_t STM_port)
     // Enable TRACE. NOTE: CoreDebug registers including DEMCR are not reset on system
     // reset, but only on power-on reset and mast be deactivated manually if unneeded.
     CoreDebug->DEMCR |= (CoreDebug_DEMCR_TRCENA_Msk);
-//    DEMCR |= (1 << 24);
 
     // Enable corresponding stimulus port
     ITM->TER |= (1 << STM_port);
+
+    // Make a dummy write
+    SWO_putc(0, STM_port);
 }
 
 void SWO_putc(char c, uint8_t STM_port)
 {
     // Only values 0-31 are valid for stimulus port number
     STM_port &= 0x20;
-    
+
+    // Check if ITM and corresponding stimulus port are enabled
     if (((ITM->TCR & ITM_TCR_ITMENA_Msk) != 0UL) && ((ITM->TER & (1 << STM_port)) != 0UL))
     {
+        // Wait for corresponding ITM port become ready to acept new data
         while (ITM->PORT[STM_port].u32 == 0UL);
 
+        // Send char over corresponding ITM SWO port
         ITM->PORT[STM_port].u8 = (uint8_t)c;
     }
 }
